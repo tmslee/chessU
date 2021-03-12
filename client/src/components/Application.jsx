ab3871a0f612747fc6985a85a33b4336d0125eaa
Alvin Ngo <ngoalvin97@gmail.com>
2021-03-12 15:37:28 -0500

    add placeholder cookie

--- client/src/components/Application.jsx
+++ client/src/components/Application.jsx
@@ -3,10 +3,18 @@ import LoginForm from "./UserAuth/LoginForm"
 
 export default function Application() {
 
+  const [cookie, setCookie] = useState(null);
+
+  if (!cookie) {
+    return (
+      <LoginForm setCookie={setCookie}/>
+    )
+  }
+
   return (
     <main>
     <h1>hi</h1>
-    <LoginForm />
+    <LoginForm setCookie={setCookie} />
     </main>
   );
 
