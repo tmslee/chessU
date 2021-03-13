715812f133d065af64a373341dd5f9ca7715c124
Sang Hyup Lee <tms.lee9@gmail.com>
2021-03-13 16:45:33 -0500

    working on timer

--- src/components/Home/AcceptTimer.jsx
+++ src/components/Home/AcceptTimer.jsx
@@ -4,26 +4,24 @@ import Timer from "react-compound-timer";
 
 export default function AcceptTimer(props) {
   const [col, setCol] = useState("success");
-  const isRunning = props.isRunning;
-  const color = props.color;
 
   return (
     <div className = "timer">
       <Alert variant={col}>
-        <Alert.Heading>{color}: {username} </Alert.Heading>
+        <Alert.Heading>"Opponent Found!"</Alert.Heading>
         <Alert.Heading>Countdown: <Timer 
           initialTime={15000}
           direction="backward"
           timeToUpdate={1000}
           chechpoints={[{time: 0, callback: () => setCol("danger")},]}
           >
-      {({ resume, pause }) => (
+          <span><Timer.Seconds /> seconds</span>
+      {/* {({ resume, pause }) => (
             <span>
-              <span><Timer.Minutes /> minutes</span>
               <span><Timer.Seconds /> seconds</span>
               {isRunning ? resume() : pause()}
             </span>
-        )}
+        )} */}
           </Timer>
         </Alert.Heading>
       </Alert>
