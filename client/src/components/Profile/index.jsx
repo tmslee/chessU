e284aaeaa7dcd5a0776bf050386d70e535bed125
Alvin Ngo <ngoalvin97@gmail.com>
2021-03-13 21:12:04 -0500

    add test update

--- client/src/components/Profile/index.jsx
+++ client/src/components/Profile/index.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import UserInfo from "./UserInfo";
 import Statistics from "./Statistics";
+import Settings from "./Settings";
 
 export default function Profile () {
 
+  const [settings, setSettings] = useState(false);
+
   return (
-    <div>
-      <UserInfo/>
-      <Statistics/>
-    </div>
+    <>
+    {settings && <Settings 
+    setSettings={setSettings}
+    />}
+    <UserInfo
+    setSettings={setSettings}
+    />
+    <Statistics/>
+    </>
   )
 }
