e6502ac9f367b8dd1b3d2ebf70020ce4438b90e0
Sang Hyup Lee <tms.lee9@gmail.com>
2021-03-14 21:25:38 -0400

    matchmaker feature

--- server/src/index.js
+++ server/src/index.js
@@ -4,32 +4,43 @@ const ENV = require("./environment");
 const app = require("./application")(ENV, { updateAppointment });
 const server = require("http").Server(app);
 
-const WebSocket = require("ws");
-const wss = new WebSocket.Server({ server });
-
-wss.on("connection", socket => {
+const io = require("socket.io")(server);
+io.on("connection", socket => {
   socket.onmessage = event => {
-    console.log(`Message Received: ${event.data}`);
-
-    if (event.data === "ping") {
+    console.log(`Message Receoved: ${event.data}`);
+    if (event.data === "ping"){
       socket.send(JSON.stringify("pong"));
     }
-  };
+  }
 });
 
-function updateAppointment(id, interview) {
-  wss.clients.forEach(function eachClient(client) {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(
-        JSON.stringify({
-          type: "SET_INTERVIEW",
-          id,
-          interview
-        })
-      );
-    }
-  });
-}
+
+// const WebSocket = require("ws");
+// const wss = new WebSocket.Server({ server });
+
+// wss.on("connection", socket => {
+//   socket.onmessage = event => {
+//     console.log(`Message Received: ${event.data}`);
+
+//     if (event.data === "ping") {
+//       socket.send(JSON.stringify("pong"));
+//     }
+//   };
+// });
+
+// function updateAppointment(id, interview) {
+//   wss.clients.forEach(function eachClient(client) {
+//     if (client.readyState === WebSocket.OPEN) {
+//       client.send(
+//         JSON.stringify({
+//           type: "SET_INTERVIEW",
+//           id,
+//           interview
+//         })
+//       );
+//     }
+//   });
+// }
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT} in ${ENV} mode.`);
