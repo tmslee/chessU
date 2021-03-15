cd21c29ed226be419deacd9bdccd211ffc207dce
HaopengSun <haopengsun2016@email.com>
2021-03-15 13:53:05 -0400

    fix socket bug

--- client/src/components/GameView/Game.jsx
+++ client/src/components/GameView/Game.jsx
@@ -23,11 +23,12 @@ function Game() {
     reset: false,
     chessmoves: [],
     winner:'',
-    roomId: 1
+    roomId: 1,
+    isReceived: true
   })
 
   const roomId = state.roomId;
-  const { currentMove, sendMove } = useMove(roomId);
+  let { currentMove, sendMove } = useMove(roomId);
 
   // set current positions from Chess.js
   let game = useRef(null);
@@ -35,53 +36,67 @@ function Game() {
     game.current = new Chess();
   }
 
-  // let rerender = useRef(true);
-
   console.log('re-render');
   console.log(currentMove);
+  console.log('before', state.isReceived);
+
+  const gameover = function(winner){
+    setState(prev => ({...prev,
+      isWhiteRunning: false,
+      isBlackRunning: false,
+      isGameOver: true,
+      modalShow: true,
+      winner
+    }));
+  }
 
   // if the move is not made by current user
   // reset the chessboard based on received move made by other user
-  if(!currentMove.movedByCurrentUser && currentMove.length !== 0){
-    console.log('not my move');
-    console.log(currentMove);
-
-    currentMove.movedByCurrentUser = true;
+  if(!currentMove.movedByCurrentUser && currentMove.length !== 0 && state.isReceived){
+    console.log('not my move', !currentMove.movedByCurrentUser);
+    
     const from = currentMove.body.from;
     const to = currentMove.body.to;
     game.current.move({ from, to });
     let moveReceived = {from, to};
-
+    
     let chessmoves = state.chessmoves;
     if(state.isBlackRunning){
       moveReceived['player'] = usernameBlack;
       chessmoves.unshift(moveReceived);
-      sendMove(moveReceived);
     } else {
       moveReceived['player'] = usernameWhite;
       chessmoves.unshift(moveReceived);
-      sendMove(moveReceived);
     }
-    
-    if (state.isWhiteRunning){
-      setState(prev => ({...prev,
-        isWhiteRunning: false,
-        isBlackRunning: true,
-        position: game.current.fen(),
-        chessmoves
-      }));
-    } else {
-      setState(prev => ({...prev,
-        isWhiteRunning: true,
-        isBlackRunning: false,
-        position: game.current.fen(),
-        chessmoves
-      }));
 
-      // console.log(rerender.current);
-      // rerender.current = false;
+    if (!game.current.game_over()){
+      if (state.isWhiteRunning){
+        setState(prev => ({...prev,
+          isWhiteRunning: false,
+          isBlackRunning: true,
+          position: game.current.fen(),
+          chessmoves,
+          isReceived: false
+        }));
+      } else {
+        setState(prev => ({...prev,
+          isWhiteRunning: true,
+          isBlackRunning: false,
+          position: game.current.fen(),
+          chessmoves,
+          isReceived: false
+        }));
+      }
+    } else {
+      if (game.current.turn() === 'b'){
+        gameover('White');
+      } else {
+        gameover('Black');
+      }
     }
   }
+
+  currentMove.movedByCurrentUser = true;
 
   // user makes a move
   //The logic to be performed on piece drop. See chessboardjsx.com/integrations for examples.
@@ -113,14 +128,16 @@ function Game() {
           isWhiteRunning: false,
           isBlackRunning: true,
           position: game.current.fen(),
-          chessmoves
+          chessmoves,
+          isReceived: true
         }));
       } else {
         setState(prev => ({...prev,
           isWhiteRunning: true,
           isBlackRunning: false,
           position: game.current.fen(),
-          chessmoves
+          chessmoves,
+          isReceived: true
         }));
       }
     } else {
@@ -130,16 +147,6 @@ function Game() {
         gameover('Black');
       }
     }
-  }
-
-  const gameover = function(winner){
-    setState(prev => ({...prev,
-      isWhiteRunning: false,
-      isBlackRunning: false,
-      isGameOver: true,
-      modalShow: true,
-      winner
-    }));
   }
   
   const setModalShow = function(bool){
