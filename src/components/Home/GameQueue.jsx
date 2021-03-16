ba3855dde65a0e4d85e108a2fdc94e9c8bfd6cc0
HaopengSun <haopengsun2016@email.com>
2021-03-16 15:40:43 -0400

    queue up testing

--- src/components/Home/GameQueue.jsx
+++ src/components/Home/GameQueue.jsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 
 import "./styles/GameQueue.scss"
 import {Button, Modal} from "react-bootstrap";
+import useQueue from "../../hooks/useQueue";
 
 const RANKED = "RANKED";
 const CASUAL = "CASUAL";
@@ -12,39 +13,41 @@ const ACCEPT_MATCH = "ACCEPT_MATCH";
 export default function GameQueue(props) {
   const {gameOptions, setGameOptions, goToView, returnToGameOptions} = props;
   
-  const [opponentID, setOpponentID] = useState(gameOptions.opponentID);
+  // const [opponentID, setOpponentID] = useState(gameOptions.opponentID);
 
-  const findOpponent = function(gameOptions) {
-    //implement this: go into db and grab an opponent from queue. returns opponentID
-    console.log("finding opponent for game: ...");
-    console.log(gameOptions);
-    return 1; 
-  };
+  const {inQueue, enqueue, dequeue} = useQueue(gameOptions);
 
-  const inviteOpponent = function (gameOptions) {
-    console.log("sending accept invitation to opponent...");
-    //actually send an invitation here
-  }
+  // const findOpponent = function(gameOptions) {
+  //   //implement this: go into db and grab an opponent from queue. returns opponentID
+  //   console.log("finding opponent for game: ...");
+  //   console.log(gameOptions);
+  //   return 1; 
+  // };
+
+  // const inviteOpponent = function (gameOptions) {
+  //   console.log("sending accept invitation to opponent...");
+  //   //actually send an invitation here
+  // }
+
+  // // useEffect(() => {
+  // //   findOpponent(gameOptions);
+  // // }, []);
 
   // useEffect(() => {
-  //   findOpponent(gameOptions);
-  // }, []);
-
-  useEffect(() => {
-    if(!opponentID) {
-      const foundOpponentID = findOpponent(gameOptions);
-      // uncomment line below if you want to use findOpponent.
-      // keep it commented if you want to change the opponent in chrome dev tools manually
-      //setOpponent(foundOpponentID);
-    }
-    else {
-      console.log(`opponent found! setting opponent as: ${opponentID}`);
-      setGameOptions({...gameOptions, opponentID});
-      console.log(`opponent set - inviting ${opponentID} to a game`);
-      inviteOpponent(gameOptions);
-      goToView(ACCEPT_MATCH);
-    }
-  }, [!]);
+  //   if(!opponentID) {
+  //     const foundOpponentID = findOpponent(gameOptions);
+  //     // uncomment line below if you want to use findOpponent.
+  //     // keep it commented if you want to change the opponent in chrome dev tools manually
+  //     //setOpponent(foundOpponentID);
+  //   }
+  //   else {
+  //     console.log(`opponent found! setting opponent as: ${opponentID}`);
+  //     setGameOptions({...gameOptions, opponentID});
+  //     console.log(`opponent set - inviting ${opponentID} to a game`);
+  //     inviteOpponent(gameOptions);
+  //     goToView(ACCEPT_MATCH);
+  //   }
+  // }, [opponentID]);
 
   return (
     <>
