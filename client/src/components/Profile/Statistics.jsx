14312ecc37374325a22c7f35b53e75b04c8ac502
Alvin Ngo <ngoalvin97@gmail.com>
2021-03-17 22:19:04 -0400

    add a state to get statsInfo

--- client/src/components/Profile/Statistics.jsx
+++ client/src/components/Profile/Statistics.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./styles/Statistics.scss"
 
 export default function Statistics(props) {
-  const {currentUser} = props;
+  const { currentUser, statsInfo } = props;
+  console.log(currentUser)
+
+
   
-  // do a get request to users/:id which gives back all stats
 
   return (
     <div className="stats-box">
       <div className="stats">
-        <a>W/L:</a>
-        <a>ELO: </a>
+        <a>W/L:  {statsInfo.data.wins.wins}/{statsInfo.data.losses.losses} </a>
+        <a>ELO:  {currentUser.elo} </a>
+        <a></a> 
       </div>
     </div>
   )
