7658751cb9a243a376d4fdf4a4a0ca3e68192d7f
HaopengSun <haopengsun2016@email.com>
2021-03-18 16:02:52 -0400

    finish ai game

--- src/components/GameView/Timer.jsx
+++ src/components/GameView/Timer.jsx
@@ -9,7 +9,7 @@ export default function Countdown(props) {
   const color = props.color;
   const isGameOver = props.isGameOver;
   const timeout = props.timeout;
-  const duration = props.duration;
+  const duration = props.duration || 30;
 
   return (
     <div className = "timer">
