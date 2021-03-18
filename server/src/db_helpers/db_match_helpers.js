2f9193ccd34832ba264ec8858ba48c779a4af066
Alvin Ngo <ngoalvin97@gmail.com>
2021-03-18 15:18:56 -0400

    refactored matches routes and helpers

--- server/src/db_helpers/db_match_helpers.js
+++ server/src/db_helpers/db_match_helpers.js
@@ -12,3 +12,41 @@ const addMatch = function(type, user1, user2) {
 
 exports.addMatch = addMatch;
 
+const allMatches = function() {
+  return db.query(`
+  SELECT * 
+  FROM matches;`
+  ).then(res => res.rows)
+};
+
+exports.allMatches = allMatches;
+
+const getMatchById = function(id) {
+  return db.query(`
+  SELECT * 
+  FROM matches 
+  where id = $1;`,
+  [id]
+  ).then(res => res.rows)
+};
+
+exports.getMatchById = getMatchById;
+
+const updateMatch = function(type, user1ID, user2ID, winner, loser, matchID) {
+  return db.query(`
+  UPDATE matches SET 
+    type = $1::text,
+    user1_id = $2::integer, 
+    user2_id = $3::integer,  
+    winner = $4::integer,
+    loser = $5::integer
+  WHERE id = $6::integer
+  RETURNING *;`,
+  [type, user1ID, user2ID, winner, loser, matchID]
+  ).then(res => res.rows)
+};
+
+exports.updateMatch = updateMatch;
+
+
+
