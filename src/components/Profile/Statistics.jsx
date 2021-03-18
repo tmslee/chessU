3fe0c91579b63e39f1977589d02bddc8b52ced21
Alvin Ngo <ngoalvin97@gmail.com>
2021-03-17 21:00:58 -0400

    merge

--- src/components/Profile/Statistics.jsx
+++ src/components/Profile/Statistics.jsx
@@ -4,12 +4,8 @@ import "./styles/Statistics.scss"
 export default function Statistics(props) {
   const {currentUser} = props;
   
-<<<<<<< HEAD
   // do a get request to users/:id which gives back all stats
 
-=======
-  
->>>>>>> e7eb234b4e10c3ff74a311aa768af7b9597f187b
   return (
     <div className="stats-box">
       <div className="stats">
