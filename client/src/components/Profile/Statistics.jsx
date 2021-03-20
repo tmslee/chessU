db931386168029d8e9cbbb807e085bc72b9e4c5a
Alvin Ngo <ngoalvin97@gmail.com>
2021-03-19 21:10:21 -0400

    add styling to stats

--- client/src/components/Profile/Statistics.jsx
+++ client/src/components/Profile/Statistics.jsx
@@ -21,12 +21,44 @@ export default function Statistics(props) {
   return (
     <div className="stats-box">
       <div className="stats">
-        <a>W/L:  {wins}/{losses} </a>
+        <table className="table-striped">
+          <thead>
+            <th scope="col"></th>
+            <th scope="col"></th>
+          </thead>
+          <tbody>
+          <tr>
+            <th scope="row">W/L</th>
+            <td>{wins}/{losses}</td>
+          </tr>
+          <tr>
+          <th scope="row">Ranked 30 Elo</th>
+            <td>{currentUser.ranked30}</td>
+          </tr>
+          <tr>
+            <th scope="row">Ranked 10 Elo</th>
+            <td>{currentUser.ranked10}</td>
+          </tr>
+          <tr>
+            <th scope="row">Casual Elo</th>
+            <td>{currentUser.casual}</td>
+          </tr>
+          <tr>
+            <th scope="row">Total Matches Played</th>
+            <td>{matches}</td>
+          </tr>
+          <tr>
+            <th scope="row">Avg Moves Per Match</th>
+            <td>{actions/matches}</td>
+          </tr>
+          </tbody>
+        </table>
+        {/* <a>W/L:  {wins}/{losses} </a>
         <a>Ranked 30 Elo: {currentUser.ranked30} </a>
         <a>Ranked 10 Elo: {currentUser.ranked10} </a>
         <a>Casual Elo: {currentUser.casual} </a>
         <a>Total Matches Played: {matches}</a>
-        <a>Avg Moves Per Match: {actions/matches} </a> 
+        <a>Avg Moves Per Match: {actions/matches} </a>  */}
         {/* <a>Percent Wins On White: {whiteWins/matches * 100} </a>  */}
         {/* <a>Percent Wins On Black: {blackWins/matches * 100} </a>  */}
         {/* <a>Avg Match Length: {avgMatch.minutes}.{avgMatch.seconds} </a> */}
