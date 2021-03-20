d82ab77bb6d42031117e83f624e4eb6ab84f0389
Alvin Ngo <ngoalvin97@gmail.com>
2021-03-19 21:09:21 -0400

    add tab buttons to switch

--- src/components/Profile/ProfileTabs.jsx
+++ src/components/Profile/ProfileTabs.jsx
@@ -1,12 +1,19 @@
-import axios from 'axios';
 import "./styles/ProfileTabs.scss";
 
 export default function ChangeAvatar (props) {
+
+  const { setTabs } = props;
   
   return (
     <div class="profile-btn-group">
-    <button>Stats</button>
-    <button>Match History</button>
+      <button onClick={() => 
+      setTabs({stats:true, history: false})}>
+        Stats
+      </button>
+      <button onClick={() => 
+      setTabs({stats:false, history: true})}>
+        Match History
+        </button>
     </div>
   )
 };
