614b980459f182692eeadf90ead4833a90dd2321
Alvin Ngo <ngoalvin97@gmail.com>
2021-03-20 12:07:03 -0400

    add bootswatch sketchy and doodle in index

--- client/src/components/Application.jsx
+++ client/src/components/Application.jsx
@@ -11,7 +11,7 @@ import LeaderBoards from "./Community/LeaderBoards";
 import useToken from "../hooks/useToken";
 import AiGame from "./AiGame/AiGame";
 import axios from 'axios';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootswatch/dist/sketchy/bootstrap.min.css";
 
 export default function Application() {
 
