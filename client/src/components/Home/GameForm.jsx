5ddf9c0406d2e54817ea4304a62b4e813b58fa87
Alvin Ngo <ngoalvin97@gmail.com>
2021-03-20 21:17:17 -0400

    import styling

--- client/src/components/Home/GameForm.jsx
+++ client/src/components/Home/GameForm.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from "react";
 import axios from 'axios';
+import "./styles/GameForm.scss";
 
 import "./styles/HomeMenu.scss";
 import {Button, Modal, Form, Row, Col} from "react-bootstrap";
