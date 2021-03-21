a31d1c288c2a5ae9fb93af7aa07f41ec09eea40c
Alvin Ngo <ngoalvin97@gmail.com>
2021-03-21 12:05:59 -0400

    add chat popup

--- client/src/components/ChatRoom/Chat.jsx
+++ client/src/components/ChatRoom/Chat.jsx
@@ -1,12 +1,15 @@
 import React, {useState} from "react";
 
-import "./ChatRoom.css";
+import "./ChatRoom.scss";
 import useChat from "../../hooks/chat";
 
 const Chat = (props) => {
   const roomId = props.roomId;
   const { messages, sendMessage } = useChat(roomId);
   const [newMessage, setNewMessage] = useState("");
+  const [style, setStyle] = useState({
+    display: 'none'
+  })
 
   const handleNewMessageChange = (event) => {
     setNewMessage(event.target.value);
@@ -16,6 +19,31 @@ const Chat = (props) => {
     sendMessage(newMessage);
     setNewMessage("");
   };
+
+  const handleChat = () => {
+    if (style.display === "none") {
+      const newStyle = {
+        display: "flex",
+        flexDirection: "column",
+        border: "3px solid black",
+        borderRadius: "15px",
+        position: "fixed",
+        width: "80%",
+        height: "80%",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        background: "white",
+        zIndex: "5"
+      }
+      setStyle(newStyle)
+    } else {
+      const newStyle = {
+        display: "none"
+      }
+      setStyle(newStyle)
+    }
+  }
 
   const messageShow = function(){
     return messages.map((message, i) => {
@@ -31,7 +59,9 @@ const Chat = (props) => {
   }
 
   return (
-    <div className="chat-room-container">
+    <div>
+    <button class="open-button" onClick={handleChat}>Chat</button>
+    <div className="chat-room-container" style={style}>
       <div className="messages-container">
         <ol className="messages-list">{messageShow()}</ol>
       </div>
@@ -43,7 +73,9 @@ const Chat = (props) => {
           className="new-message-input-field"
         />
         <button onClick={handleSendMessage} type="button" className="btn btn-outline-success send-message-button">Send</button>
+        <button onClick={handleChat} type="button" className="btn btn-outline-success send-message-button">Close</button>
       </div>
+    </div>
     </div>
   );
 };
