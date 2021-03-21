3b79c8c6bdb2b577dff887f871b6f18a59358427
HaopengSun <haopengsun2016@email.com>
2021-03-21 16:39:30 -0400

    updated elo shows in the popup window when game is over

--- src/components/GameView/PopupWin.jsx
+++ src/components/GameView/PopupWin.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 
 import { Button, Modal } from 'react-bootstrap';
@@ -6,6 +6,9 @@ import { Button, Modal } from 'react-bootstrap';
 export default function PopupWin(props){
   const winner = props.winner;
   const isReceivedResign = props.isReceivedResign;
+  const currentUserNotWins = props.currentUserNotWins;
+  const currentUserWins = props.currentUserWins;
+  const isWin = props.isWin;
   const result = function(winner){
     if (winner){
       return (winner + ' wins!')
@@ -19,6 +22,13 @@ export default function PopupWin(props){
       aria-labelledby="contained-modal-title-vcenter"
       centered>
       <Modal.Header closeButton><h4>Game Over! {result(winner)} {isReceivedResign && "Your opponent resigned."}</h4></Modal.Header>
+      <Modal.Body>
+        {isWin ? 
+        <><p>winner: {currentUserWins.winnerName} origin elo:{currentUserWins.winner_elo} new elo: {currentUserWins.updatedWinner}</p>
+        <p>loser: {currentUserWins.loserName} origin elo:{currentUserWins.loser_elo} new elo: {currentUserWins.updatedLoser}</p></> : 
+        <><p>winner: {currentUserNotWins.winnerName} origin elo:{currentUserNotWins.winner_elo} new elo: {currentUserNotWins.updatedWinner}</p>
+        <p>loser: {currentUserNotWins.loserName} origin elo:{currentUserNotWins.loser_elo} new elo: {currentUserNotWins.updatedLoser}</p></>}
+      </Modal.Body>
       <Modal.Footer>
         <Link to={{pathname: "/"}} >
           <Button variant="outline-info" type="button">Back to Home</Button>
