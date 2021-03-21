07281e5b7997f0082a11358d6d74ca34c2bdd7af
HaopengSun <haopengsun2016@email.com>
2021-03-21 16:49:18 -0400

    add updated elo in popup win

--- src/hooks/eloUpdate.js
+++ src/hooks/eloUpdate.js
@@ -12,7 +12,7 @@ const eloUpdate = function(winnerName, winner_elo, loserName, loser_elo){
   const updatedWinner = Math.round(winner_elo + 32 * (1 - expectedScoreWinner));
   const updatedLoser = Math.round(loser_elo + 32 * (0 - expectedScoreLoser));
 
-  return {winnerName, winner_elo, updatedWinner, loserName, winner_elo, updatedLoser};
+  return {winnerName, winner_elo, updatedWinner, loserName, loser_elo, updatedLoser};
 }
 
 exports.eloUpdate = eloUpdate;
