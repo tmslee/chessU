d3b562452bb5946ab8c375a3c59c437b9a7cd379
Sang Hyup Lee <tms.lee9@gmail.com>
2021-03-23 20:59:53 -0400

    changed seed elo for production

--- server/src/db/schema/production.sql
+++ server/src/db/schema/production.sql
@@ -1,11 +1,11 @@
 INSERT INTO users (username, email, profile_img, password, ranked30, ranked10, casual) VALUES (
-  'alvin', 'alvin@gmail.com','https://www.w3schools.com/w3images/avatar2.png', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 1800, 1200, 1700
+  'alvin', 'alvin@gmail.com','https://www.w3schools.com/w3images/avatar2.png', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 1900, 1200, 1700
 );
 INSERT INTO users (username, email, password, ranked30, ranked10, casual) VALUES (
   'thomas', 'thomas@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 900, 1200, 1800
 );
 INSERT INTO users (username, email, password, ranked30, ranked10, casual) VALUES (
-  'haopeng', 'haopeng@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 1400, 1200, 900
+  'haopeng', 'haopeng@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 2300, 1200, 900
 );
 INSERT INTO users (username, email, password, ranked30, ranked10, casual) VALUES (
   'user4', 'user4@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 1500, 1600, 1700
